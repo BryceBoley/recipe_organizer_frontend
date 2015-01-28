b085ae0c7efdfe3910d1746b6c88b757af738996
Bryce Boley <bryceboley1@gmail.com>
2015-01-28 16:32:56 -0700

    Got delete button to work

--- app/recipes/recipes.js
+++ app/recipes/recipes.js
@@ -2,21 +2,17 @@
 
 angular.module('myApp.recipes', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/recipes', {
-    templateUrl: 'recipes/recipes.html',
-    controller: 'RecipesCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/recipes', {
+            templateUrl: 'recipes/recipes.html',
+            controller: 'RecipesCtrl'
+        });
+    }])
 
-.controller('RecipesCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
-    Restangular.all('recipes').getList().then(function(recipes) {
-        $scope.recipes = recipes;
-    });
+    .controller('RecipesCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
+        Restangular.all('recipes').getList().then(function(recipes) {
+            $scope.recipes = recipes;
+        });
 
-    $scope.deleteRecipe = function(recipeID) {
-        Restangular.one('recipes', recipeID).customDELETE().then(function(){
-            $location.path('/recipes');
-        })
-    }
-}]);
+
+    }]);
