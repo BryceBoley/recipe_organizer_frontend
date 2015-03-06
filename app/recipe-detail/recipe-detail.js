c15105dd586771bbe620bf5a060b4c42382f73b3
Bryce Boley <bryceboley1@gmail.com>
2015-03-05 21:50:21 -0700

    got add and edit photo functionality, still required to enter photo when doing and edit, need to fix issue

--- app/recipe-detail/recipe-detail.js
+++ app/recipe-detail/recipe-detail.js
@@ -9,7 +9,7 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
   });
 }])
 
-.controller('RecipeDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', function($scope, Restangular, $routeParams, $location) {
+.controller('RecipeDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', '$http', function($scope, Restangular, $routeParams, $location, $http) {
 
         $scope.recipeId = $routeParams.recipeId;
 
@@ -34,19 +34,36 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
         };
 
         $scope.editRecipe = function () {
-            Restangular.one('recipes', $scope.recipeId).customPUT($scope.recipe).then(function() {
+            var boundary = "---------------------------7da24f2e50046";
+            var fd = new FormData();
+            fd.append("photo", $scope.recipe.photo);
+            fd.append("name", $scope.recipe.name);
+            fd.append("description", $scope.recipe.description);
+            fd.append("directions", $scope.recipe.directions);
+            fd.append("ingredients", $scope.recipe.ingredients);
+            fd.append("tags", $scope.recipe.tags);
+
+            console.log($scope.recipe.id);
+
+            $http.put('http://localhost:8002/recipes/' + $scope.recipe.id, fd, {
+                headers: {'Content-type': undefined },
+                transformRequest: angular.identity
+
+            }).success(function () {
+                $location.path('/recipes');
+            }).error(function (response) {
+                console.log('Error response: ' + response);
+            })};
+
+            $scope.uploadFile = function (files) {
+                $scope.recipe.photo = files[0];
+                console.log($scope.recipe.photo);
+            };
+
+         $scope.cancel = function () {
                 $location.path('/recipes');
 
-                },
-                function () {
-                    alert('There was a problem updating your recipe.')
-                })
-        };
+         }
+    }]);
 
-        $scope.cancel = function () {
-            $location.path('/recipes');
-
-
-        };
-
-}]);
+//'multipart/form-data; boundary'
