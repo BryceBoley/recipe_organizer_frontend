852894e8ea62f9da1d9e848dd01977d1586c944b
Bryce Boley <bryceboley1@gmail.com>
2015-03-10 11:52:37 -0600

    more changes to frontend

--- app/recipe-detail/recipe-detail.js
+++ app/recipe-detail/recipe-detail.js
@@ -39,7 +39,7 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
             fd.append("ingredients", $scope.recipe.ingredients);
             fd.append("tags", $scope.recipe.tags);
 
-            $http.put('/api/recipes/' + $scope.recipe.id, fd, {
+            $http.put('/recipes/' + $scope.recipe.id, fd, {
                 headers: {'Content-type': undefined },
                 transformRequest: angular.identity
 
