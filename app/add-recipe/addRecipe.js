caa7e010c26deba0540b09f2adcdd9dcdf076915
Bryce Boley <bryceboley1@gmail.com>
2015-03-11 20:09:54 -0600

    add and edit using restangular

--- app/add-recipe/addRecipe.js
+++ app/add-recipe/addRecipe.js
@@ -39,41 +39,42 @@ angular.module('myApp.addRecipe', ['ngRoute'])
             Restangular.all('recipes/').customPOST($scope.recipe).then(function () {
                     alert("Your recipe was successfully created");
                     $location.path('/recipes');
-            })},
+            })}
 
         //Add a new recipe, alert the user when it's been created or when there was a problem.
-    //    $scope.addRecipe = function () {
-    //        var boundary = "---------------------------7da24f2e50046";
-    //        var fd = new FormData();
-    //        fd.append("photo", $scope.recipe.photo);
+        $scope.addRecipe = function () {
+            var boundary = "---------------------------7da24f2e50046";
+            var fd = new FormData();
+            fd.append("photo", $scope.recipe.photo);
     //        fd.append("name", $scope.recipe.name);
     //        fd.append("description", $scope.recipe.description);
     //        fd.append("directions", $scope.recipe.directions);
     //        fd.append("ingredients", $scope.recipe.ingredients);
     //        fd.append("tags", $scope.recipe.tags);
     //
-    //        console.log(fd);
-    //
-    //        $http.post('/recipes/', fd, {
-    //            headers: {'Content-type': 'multipart/form-data; boundary'},
-    //            transformRequest: angular.identity
-    //
-    //        }).success(function () {
-    //            $location.path('/recipes');
-    //        }).error(function (response) {
-    //            console.log('Error response: ' + response);
-    //        })};
-    //
-    //
-    //    $scope.uploadFile = function (files) {
-    //        $scope.recipe.photo = files[0];
-    //        console.log($scope.recipe.photo);
-    //    };
-    //
-    //    $scope.convertImageUrl = function (url) {
-    //        return url.replace(/http:.*media/, '/api/media');
-    //    };
-    //
+            console.log(fd);
+
+            $http.post('/recipes/', fd, {
+                headers: {'Content-type': 'multipart/form-data; boundary'},
+                transformRequest: angular.identity
+
+            }).success(function () {
+                $location.path('/recipes');
+            }).error(function (response) {
+                console.log('Error response: ' + response);
+            })};
+
+
+        $scope.uploadFile = function (files) {
+            $scope.recipe.photo = files[0];
+            console.log($scope.recipe.photo);
+        };
+
+        $scope.convertImageUrl = function (url) {
+            return url.replace(/http:.*media/, '/api/media');
+
+        };
+
         $scope.cancel = function () {
              $location.path('/recipes');
 
