6a88973dbc0db72d4013c413b7f8cccd1a2f3c03
Bryce Boley <bryceboley1@gmail.com>
2015-04-06 11:22:41 -0600

    changed to add recipe, not working presently

--- app/add-recipe/addRecipe.js
+++ app/add-recipe/addRecipe.js
@@ -34,54 +34,54 @@ angular.module('myApp.addRecipe', ['ngRoute'])
 
         };
 
-        $scope.addRecipe = function () {
-            var fd = new FormData();
-            angular.forEach($scope.recipe, function (){
-                fd.append("photo", $scope.recipe.photo);
-                fd.append("name", $scope.recipe.name);
-                fd.append("description", $scope.recipe.description);
-                fd.append("directions", $scope.recipe.directions);
-                fd.append("ingredients", $scope.recipe.ingredients);
-                fd.append("tags", $scope.recipe.tags);
-                console.log('fd')
-            });
-
-            Restangular.one('recipes/', $scope.recipeId).customPOST($scope.recipeId).then(function () {
-                        $location.path('/recipes');
-
-                    alert('nice job');
-                        $location.path('/recipes')
-
-                    .error(function (response) {
-                        console.log('Error response: ' + response);
-            })
-        })};
-
-
-        ////Add a new recipe, alert the user when it's been created or when there was a problem.
         //$scope.addRecipe = function () {
-        //    var boundary = "---------------------------7da24f2e50046";
         //    var fd = new FormData();
-        //    fd.append("photo", $scope.recipe.photo);
-        //    fd.append("name", $scope.recipe.name);
-        //    fd.append("description", $scope.recipe.description);
-        //    fd.append("directions", $scope.recipe.directions);
-        //    fd.append("ingredients", $scope.recipe.ingredients);
-        //    fd.append("tags", $scope.recipe.tags);
-    //
-    //        console.log(fd);
-    //
-    //        $http.post('/recipes', fd, {
-    //            headers: {'Content-type': 'image/jpeg'},
-    //            transformRequest: angular.identity
-    //
-    //        }).success(function () {
-    //            $location.path('/recipes');
-    //        }).error(function (response) {
-    //            console.log('Error response: ' + response);
-    //        })};
-    //
-    //
+        //    angular.forEach($scope.recipe, function (){
+        //        fd.append("photo", $scope.recipe.photo);
+        //        fd.append("name", $scope.recipe.name);
+        //        fd.append("description", $scope.recipe.description);
+        //        fd.append("directions", $scope.recipe.directions);
+        //        fd.append("ingredients", $scope.recipe.ingredients);
+        //        fd.append("tags", $scope.recipe.tags);
+        //        console.log('fd')
+        //    });
+        //
+        //    Restangular.one('recipes/', $scope.recipeId).customPOST($scope.recipeId).then(function () {
+        //                $location.path('/recipes');
+        //
+        //            alert('nice job');
+        //                $location.path('/recipes')
+        //
+        //            .error(function (response) {
+        //                console.log('Error response: ' + response);
+        //    })
+        //})};
+
+
+        //Add a new recipe, alert the user when it's been created or when there was a problem.
+        $scope.addRecipe = function () {
+            var boundary = "---------------------------7da24f2e50046";
+            var fd = new FormData();
+            //fd.append("photo", $scope.recipe.photo);
+            fd.append("name", $scope.recipe.name);
+            fd.append("description", $scope.recipe.description);
+            fd.append("directions", $scope.recipe.directions);
+            fd.append("ingredients", $scope.recipe.ingredients);
+            fd.append("tags", $scope.recipe.tags);
+
+            console.log(fd);
+
+            $http.post('/recipes', fd, {
+                headers: {'Content-type': 'image/jpeg'},
+                transformRequest: angular.identity
+
+            }).success(function () {
+                $location.path('/recipes');
+            }).error(function (response) {
+                console.log('Error response: ' + response);
+            })};
+
+
         $scope.uploadFile = function (files) {
             $scope.recipe.photo = files[0];
             console.log($scope.recipe.photo);
