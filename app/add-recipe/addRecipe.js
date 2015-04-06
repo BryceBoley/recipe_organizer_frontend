8d86b35409b7e872170ed6b4ba1c68173f9d1676
Bryce Boley <bryceboley1@gmail.com>
2015-04-06 16:46:59 -0600

    added correct url on add recipes

--- app/add-recipe/addRecipe.js
+++ app/add-recipe/addRecipe.js
@@ -47,7 +47,7 @@ angular.module('myApp.addRecipe', ['ngRoute'])
 
             console.log(fd);
 
-            $http.post('http://localhost:8002/recipes/', fd, {
+            $http.post('/recipes', fd, {
                 headers: {'Content-type': undefined },
                 transformRequest: angular.identity
 
