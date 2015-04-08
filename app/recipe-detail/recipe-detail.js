5243e350cdb08ecae451c9d49d7d217644a06d1a
Bryce Boley <bryceboley1@gmail.com>
2015-04-07 18:03:39 -0600

    changes to add edit, no nested yet

--- app/recipe-detail/recipe-detail.js
+++ app/recipe-detail/recipe-detail.js
@@ -1,90 +1,3 @@
-//'use strict';
-//
-//angular.module('myApp.recipeDetail', ['ngRoute'])
-//
-//    .config(['$routeProvider', function($routeProvider) {
-//        $routeProvider.when('/recipes/:recipeId', {
-//            templateUrl: 'recipe-detail/recipe-detail.html',
-//            controller: 'RecipeDetailCtrl'
-//        });
-//    }])
-//
-//    .controller('RecipeDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', '$http', function($scope, Restangular, $routeParams, $location) {
-//
-//        $scope.recipeId = $routeParams.recipeId;
-//
-//        Restangular.one('recipes', $scope.recipeId).customGET().then(function (data) {
-//            $scope.recipe = data;
-//
-//
-//        });
-//
-//
-//        $scope.deleteRecipe = function () {
-//            var confirmation = confirm('Are you sure you want to delete this recipe? This cannot be undone');
-//
-//            if (confirmation) {
-//                Restangular.one('recipes', $scope.recipeId).customDELETE().then(function () {
-//                        $location.path('/recipes');
-//                    },
-//                    function () {
-//                        alert('There was a problem deleting your recipe')
-//                    })
-//            }
-//        };
-//
-//        //Add the ingredients to the recipe object we're building
-//        $scope.addIngredientToRecipe = function(ingredientName) {
-//            var ingredient = {name: ingredientName};
-//            $scope.recipe.ingredients.push(ingredient);
-//            $scope.ingredientName = '';
-//
-//        };
-//
-//        // Add the tags to the recipe object we're building
-//        $scope.addTagToRecipe = function (tagName) {
-//            var tag = {name: tagName};
-//            $scope.recipe.tags.push(tag);
-//            $scope.tagName = '';
-//
-//        };
-//
-//
-//        $scope.editRecipe = function () {
-//            var fd = new FormData();
-//            fd.append("photo", $scope.recipe.photo);
-//            fd.append("name", $scope.recipe.name);
-//            fd.append("description", $scope.recipe.description);
-//            fd.append("ingredients", $scope.recipe.ingredients);
-//            fd.append("tags", $scope.recipe.tags);
-//
-//            $http.put('http://localhost:8002/recipes/' + $scope.recipe.id, fd, {
-//                    headers: {'Content-type': undefined },
-//                    transformRequest: angular.identity
-//
-//                }).success(function () {
-//                    $location.path('/recipes');
-//                }).error(function (response) {
-//                    console.log('Error response: ' + response);
-//                })};
-//
-//
-//
-//
-//        $scope.cancel = function () {
-//            $location.path('/recipes');
-//
-//
-//        };
-//
-//    }]);
-
-
-
-
-
-
-
 'use strict';
 
 angular.module('myApp.recipeDetail', ['ngRoute'])
@@ -96,17 +9,16 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
         });
     }])
 
-    .controller('RecipeDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', function($scope, Restangular, $routeParams, $location, $http) {
+    .controller('RecipeDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', '$http', function($scope, Restangular, $routeParams, $location, $http) {
 
         $scope.recipeId = $routeParams.recipeId;
 
         Restangular.one('recipes', $scope.recipeId).customGET().then(function (data) {
             $scope.recipe = data;
+
+
         });
 
-        //$scope.convertImageUrl = function (url) {
-        //    return url.replace(/http:.*media/, '/api/media');
-        //};
 
         $scope.deleteRecipe = function () {
             var confirmation = confirm('Are you sure you want to delete this recipe? This cannot be undone');
@@ -121,7 +33,7 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
             }
         };
 
-        // Add the ingredients to the recipe object we're building
+        //Add the ingredients to the recipe object we're building
         $scope.addIngredientToRecipe = function(ingredientName) {
             var ingredient = {name: ingredientName};
             $scope.recipe.ingredients.push(ingredient);
@@ -137,32 +49,120 @@ angular.module('myApp.recipeDetail', ['ngRoute'])
 
         };
 
+
         $scope.editRecipe = function () {
-            Restangular.one('recipes', $scope.recipeId).customPUT($scope.recipe).then(function() {
-                $location.path('/recipes');
+            var fd = new FormData();
+            fd.append("photo", $scope.recipe.photo);
+            fd.append("name", $scope.recipe.name);
+            fd.append("description", $scope.recipe.description);
+            fd.append("ingredients", $scope.recipe.ingredients);
+            fd.append("tags", $scope.recipe.tags);
 
-            },
-            function () {
-                alert('There was a problem updating your recipe.')
-            })
-        };
+            $http.put('http://localhost:8002/recipes/' + $scope.recipe.id, fd, {
+                    headers: {'Content-type': undefined },
+                    transformRequest: angular.identity
 
-        $scope.cancel = function () {
-            $location.path('/recipes');
+                }).success(function () {
+                    $location.path('/recipes');
+                }).error(function (response) {
+                    console.log('Error response: ' + response);
+                })};
 
 
-        };
 
-        $scope.uploadFile = function (files) {
-            $scope.recipe.photo = files[0];
-        };
-
-        //$scope.convertImageUrl = function (url) {
-        //    return url.replace(/http:.*media/, '/api/media');
-        //};
 
         $scope.cancel = function () {
             $location.path('/recipes');
 
-        }
+
+        };
+
     }]);
+
+
+
+
+
+
+//
+//'use strict';
+//
+//angular.module('myApp.recipeDetail', ['ngRoute'])
+//
+//    .config(['$routeProvider', function($routeProvider) {
+//        $routeProvider.when('/recipes/:recipeId', {
+//            templateUrl: 'recipe-detail/recipe-detail.html',
+//            controller: 'RecipeDetailCtrl'
+//        });
+//    }])
+//
+//    .controller('RecipeDetailCtrl', ['$scope', 'Restangular', '$routeParams', '$location', function($scope, Restangular, $routeParams, $location, $http) {
+//
+//        $scope.recipeId = $routeParams.recipeId;
+//
+//        Restangular.one('recipes', $scope.recipeId).customGET().then(function (data) {
+//            $scope.recipe = data;
+//        });
+//
+//        $scope.convertImageUrl = function (url) {
+//            return url.replace(/http:.*media/, '/api/media');
+//        };
+//
+//        $scope.deleteRecipe = function () {
+//            var confirmation = confirm('Are you sure you want to delete this recipe? This cannot be undone');
+//
+//            if (confirmation) {
+//                Restangular.one('recipes', $scope.recipeId).customDELETE().then(function () {
+//                        $location.path('/recipes');
+//                    },
+//                    function () {
+//                        alert('There was a problem deleting your recipe')
+//                    })
+//            }
+//        };
+//
+//        // Add the ingredients to the recipe object we're building
+//        $scope.addIngredientToRecipe = function(ingredientName) {
+//            var ingredient = {name: ingredientName};
+//            $scope.recipe.ingredients.push(ingredient);
+//            $scope.ingredientName = '';
+//
+//        };
+//
+//        // Add the tags to the recipe object we're building
+//        $scope.addTagToRecipe = function (tagName) {
+//            var tag = {name: tagName};
+//            $scope.recipe.tags.push(tag);
+//            $scope.tagName = '';
+//
+//        };
+//
+//        $scope.editRecipe = function () {
+//            Restangular.one('recipes', $scope.recipeId).customPUT($scope.recipe).then(function() {
+//                $location.path('/recipes');
+//
+//            },
+//            function () {
+//                alert('There was a problem updating your recipe.')
+//            })
+//        };
+//
+//        $scope.cancel = function () {
+//            $location.path('/recipes');
+//
+//
+//        };
+//
+//        $scope.uploadFile = function (files) {
+//            $scope.recipe.photo = files[0];
+//        };
+//
+//        $scope.convertImageUrl = function (url) {
+//            return url.replace(/http:.*media/, '/api/media');
+//        };
+//
+//        $scope.cancel = function () {
+//            $location.path('/recipes');
+//
+//        }
+//    }]);
